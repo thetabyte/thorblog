d2413cfbe6ec1350404321c203b3eae3d1f7b623
thetabyte <seunlatunde@gmail.com>
2020-09-23 11:30:42 +0100

    thorblog implementation initial commit

---
+++ app/database/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db_name = 'orion';
+
+$conn = new mysqli($host, $user, $pass, $db_name);
+
+if ($conn->connect_error){
+    die('Database connection error: '. $conn->connect_error);
+} else {
+    echo "Db connection successful";
+}
