28f6473536086f1a5b864e2fec7aba3207774de4
thetabyte <seunlatunde@gmail.com>
2021-01-08 20:10:10 -0800

    chore: general modification to codebase

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -2,18 +2,19 @@
         <a class="logo" href="<?php echo BASE_URL . '/index.php'; ?>">
             <h1 class="logo-text"><span>Bifrost</span>Daily</h1>
         </a>
-         <i class="fa fa-bars menu-toggle"></i>   
+        <i class="fa fa-bars menu-toggle"></i>   
         <ul class="nav">
-            
-            <li>
-                <a href="#">
-                    <i class="fa fa-user"></i>
-                    Ilsa Faust <!--Locked in user, above buttons commented out to focus on styling user item-->
-                    <i class="fa fa-chevron-down" style="font-size: .8em;"></i> <!--styling ideally should be in style sheet target ul list and place styling-->
-                </a> 
-                <ul>
-                    <li><a href="#" class="logout">Logout</a></li>
-                </ul>
-            </li> 
+            <?php if (isset($_SESSION['id'])) { ?> <!--you can check for username or id with $_SESSION['user/id'] to display current user-->
+                <li>
+                    <a href="#">
+                        <i class="fa fa-user"></i>
+                        <?php echo $_SESSION['username']; ?>  
+                        <i class="fa fa-chevron-down" style="font-size: .8em;"></i> <!--styling ideally should be in style sheet target ul list and place styling-->
+                    </a> 
+                    <ul>
+                        <li><a href="<?php echo BASE_URL . "/logout.php"; ?>" class="logout">Logout</a></li>
+                    </ul>
+                </li> 
+            <?php } ?>
         </ul>
     </header>
--- index.php
+++ index.php
@@ -1,213 +1,142 @@
 <?php 
-include('path.php'); 
+include("path.php"); 
 include(ROOT_PATH . "/app/controllers/topics.php");
 
+$posts = array();
+$postsTitle = 'Recent Posts';
+
+if (isset($_GET['top_id'])) {
+    $posts = getPostTopicId($_GET['top_id']);
+    $postsTitle = "Showing search results for posts under '" . $_GET['name'] . "'";
+}else if (isset($_POST['search-term'])) {
+    $postsTitle = "Showing search results for '" . $_POST['search-term'] . "'";
+    $posts = searchPosts($_POST['search-term']);
+} else {
+    $posts = getPublishedPosts();//selectAll('posts', ['published' => 1]); || NoteTS: because i published "ie; bool -> 1" only 1 post at creation it only dipslayed that even though i had other posts in the database    
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script src="https://use.fontawesome.com/1f736332cf.js"></script>
-    
-    <link href="https://fonts.googleapis.com/css2?family=Karla:ital@1&family=Lora:ital,wght@1,500&display=swap" rel="stylesheet">
-
-
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-
-    <title>BifrostDaily</title>
-</head>
-<body>
-    
-    <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
-    <?php include(ROOT_PATH . "/app/includes/msg.php"); ?>
+        <script src="https://use.fontawesome.com/1f736332cf.js"></script>
+        
+        <link href="https://fonts.googleapis.com/css2?family=Karla:ital@1&family=Lora:ital,wght@1,500&display=swap" rel="stylesheet">
 
 
+        <link rel="stylesheet" href="assets/css/style.css">
+        
 
-    <!--Page wrapper-container surrounding website content-->
+        <title>BifrostDaily</title>
+    </head>
+    <body>
+        
+        <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+        <?php include(ROOT_PATH . "/app/includes/msg.php"); ?>
 
-    <div class="page-wrapper">
 
-        <!--carousel/post-slider-->
-        <div class="post-slider">
-            <h1 class="slider-title">Trending</h1>
-            <i class="fa fa-chevron-left prev"></i>
-            <i class="fa fa-chevron-right next"></i>
-            
-            <div class="post-wrapper">
-                <div class="post">
-                    <img src="assets/images/bdblog.jpg" alt="" class="slider-image">
-                    <div class="post-info">
-                        <h4><a href="single.html">Always keep moving forward</a></h4>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar">Sep 12, 2014</i>
-                    </div>
+
+        <!--Page wrapper-container surrounding website content-->
+
+        <div class="page-wrapper">
+
+            <!--carousel/post-slider-->
+            <div class="post-slider">
+                <h1 class="slider-title">Trending</h1>
+                <i class="fa fa-chevron-left prev"></i>
+                <i class="fa fa-chevron-right next"></i>
+                
+                <div class="post-wrapper">
+
+                    <?php foreach ($posts as $post) { ?>
+                        <div class="post">
+                            <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" alt="" class="slider-image">
+                            <div class="post-info">
+                                <h4><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h4>
+                                <i class="fa fa-user"> <?php echo $post['username']; ?></i>
+                                &nbsp;
+                                <i class="fa fa-calendar"><?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+                            </div>
+                        </div>
+                    <?php } ?>
+
+
+                </div>
+            </div>
+
+
+            <!--/carousel-->
+
+            <!--main content for site-->
+
+            <div class="content clearfix">
+
+                <!--main content-->
+
+                <div class="main-content">
+                    <h1 class="recent-post-title"><?php echo $postsTitle ?></h1>
+
+                    <?php foreach ($posts as $post) { ?>
+                        <div class="post clearfix">
+                            <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" alt="" class="post-image">
+                            <div class="post-preview">
+                                <h2><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+                                <i class="fa fa-user"> <?php echo $post['username']; ?></i>
+                                &nbsp;
+                                <i class="fa fa-calendar"> <?php echo date('F j, Y', strtotime($post['created_at'])); ?></i></i>
+                                <p class="preview-text">
+                                    <?php echo html_entity_decode(substr($post['body'], 0, 150) . '...'); ?>
+                                </p>
+                                <a href="single.php?id=<?php echo $post['id']; ?>" class="btn read-more">Read More</a>
+                            </div>
+                        </div>
+                    <?php } ?>
+
                 </div>
 
-                <div class="post">
-                    <img src="assets/images/bdblog.jpg" alt="" class="slider-image">
-                    <div class="post-info">
-                        <h4><a href="single.html">Always keep moving forward</a></h4>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar">Sep 12, 2014</i>
-                    </div>
-                </div>
+                <!--/maincontent-->
 
-                <div class="post">
-                    <img src="assets/images/bdblog.jpg" alt="" class="slider-image">
-                    <div class="post-info">
-                        <h4><a href="single.html">Always keep moving forward</a></h4>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar">Sep 12, 2014</i>
-                    </div>
-                </div>
+                <div class="sidebar">
 
-                <div class="post">
-                    <img src="assets/images/bdblog.jpg" alt="" class="slider-image">
-                    <div class="post-info">
-                        <h4><a href="single.html">Always keep moving forward</a></h4>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar">Sep 12, 2014</i>
+                    <div class="section search">
+                        <h2 class="section-title">Search</h2>
+                        <form action="index.php" method="post">
+                            <input type="text" name="search-term" class="text-input" placeholder="Search...">
+                        </form>
                     </div>
-                </div>
 
-                <div class="post">
-                    <img src="assets/images/bdblog.jpg" alt="" class="slider-image">
-                    <div class="post-info">
-                        <h4><a href="single.html">Always keep moving forward</a></h4>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar">Sep 12, 2014</i>
+                    <div class="section topics">
+                        <h2 class="section-title">Topics</h2>
+                        <ul>
+                            <?php foreach ($topics as $key => $topic) { ?>
+                                <li><a href="<?php echo BASE_URL . '/index.php?top_id=' . $topic['id'] . '&name=' . $topic['name'] ?>"><?php echo $topic['name']; ?></a></li>
+                            <?php } ?>
+                        </ul>
                     </div>
+
                 </div>
 
             </div>
+
+            <!--/content-->
         </div>
 
+        <!--/container-->
 
-        <!--/carousel-->
+        <!--Footer-->
+        <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
-        <!--main content for site-->
+        <!--JQUERY-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
 
-        <div class="content clearfix">
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
-            <!--main content-->
+        <!--script-->
+        <script src="assets/js/script.js"></script>
 
-            <div class="main-content">
-                <h1 class="recent-post-title">Recent Posts</h1>
-                <div class="post clearfix">
-                    <img src="assets/images/bdblog.jpg" alt="" class="post-image">
-                    <div class="post-preview">
-                        <h2><a href="single.html">Any given Monday</a></h2>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar"> Jan 22, 2017</i>
-                        <p class="preview-text">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Voluptas, dolores consectetur nemo eaque commodi alias.
-                        </p>
-                        <a href="single.html" class="btn read-more">Read More</a>
-                    </div>
-                </div>
-                <div class="post clearfix">
-                    <img src="assets/images/bdblog2.jpg" alt="" class="post-image">
-                    <div class="post-preview">
-                        <h2><a href="single.html">Coffee-Commuting kinda morning</a></h2>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar"> Jan 22, 2017</i>
-                        <p class="preview-text">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Voluptas, dolores consectetur nemo eaque commodi alias.
-                        </p>
-                        <a href="single.html" class="btn read-more">Read More</a>
-                    </div>
-                </div>
-                <div class="post clearfix">
-                    <img src="assets/images/bdblog3.jpg" alt="" class="post-image">
-                    <div class="post-preview">
-                        <h2><a href="single.html">All work and more work</a></h2>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar"> Jan 22, 2017</i>
-                        <p class="preview-text">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Voluptas, dolores consectetur nemo eaque commodi alias.
-                        </p>
-                        <a href="single.html" class="btn read-more">Read More</a>
-                    </div>
-                </div>
-                <div class="post clearfix">
-                    <img src="assets/images/bdragnarok.jpg" alt="" class="post-image">
-                    <div class="post-preview">
-                        <h2><a href="single.html">The mystic of Ragnarok</a></h2>
-                        <i class="fa fa-user"> Ilsa Faust</i>
-                        &nbsp;
-                        <i class="fa fa-calendar"> Jan 22, 2017</i>
-                        <p class="preview-text">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Voluptas, dolores consectetur nemo eaque commodi alias.
-                        </p>
-                        <a href="single.html" class="btn read-more">Read More</a>
-                    </div>
-                </div>
-            </div>
-
-            <!--/maincontent-->
-
-            <div class="sidebar">
-
-                <div class="section search">
-                    <h2 class="section-title">Search</h2>
-                    <form action="blog.html" method="post">
-                        <input type="text" name="search-term" class="text-input" placeholder="Search...">
-                    </form>
-                </div>
-
-                <div class="section topics">
-                    <h2 class="section-title">Topics</h2>
-                    <ul>
-                        <?php foreach ($topics as $key => $topic) { ?>
-                            <li><a href="#"><?php echo $topic['name']; ?></a></li>
-                        <?php } ?>
-
-                        <!--<li><a href="#">Poems</a></li>
-                        <li><a href="#">Quotes</a></li>
-                        <li><a href="#">Fiction</a></li>
-                        <li><a href="#">Biography</a></li>
-                        <li><a href="#">Motivation</a></li>
-                        <li><a href="#">Inspiration</a></li>
-                        <li><a href="#">Life Lessons</a></li>-->
-                    </ul>
-                </div>
-
-            </div>
-
-        </div>
-
-        <!--/content-->
-
-
-    </div>
-
-    <!--/container-->
-
-    <!--Footer-->
-    <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
-
-    <!--JQUERY-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
-    <!--script-->
-    <script src="assets/js/script.js"></script>
-
-</body>
+    </body>
 </html>
